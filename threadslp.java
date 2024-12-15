04ba805f15265197e706f1f34265c0c41421f9a5
Deepak8096 <archakamdeepak81@gmail.com>
2024-12-15 13:48:35 +0530

    Add files via upload

---
+++ threadslp.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args){
+        System.out.println("Thread started...");
+        try
+        {
+            Thread.sleep(5000);
+        }catch(InterruptedException e)
+        {
+            System.out.println("Interrupted");
+        }
+        System.out.println("Thread Resumed");
+    }
+}
